ca84ff143554e5a0f372988e45de0268dc4be96e
parameswari.bala <parameswaribala@gmail.com>
2023-12-11 16:47:44 +0530

    updated

--- day1/missingdataimputer.py
+++ day1/missingdataimputer.py
@@ -10,17 +10,17 @@ import numpy as np
 
 df=pd.read_csv("F:/verizonmldec2023/datasets/Data.csv")
 
-X=df.iloc[:,1:3].values
+X = df.iloc[:, :-1].values
 
 print("Data set",df)
 
 #handle missing data
 
+# Taking care of missing data
 from sklearn.impute import SimpleImputer
 
-simpleImputer=SimpleImputer(missing_values=np.nan,strategy="mean")
-print("X values", X)
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean',verbose=0)
 
-simpleImputer = simpleImputer.fit(X[:,1:3])
-X[:, 1:3] = simpleImputer.transform(X[:,1:3])
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 print(X)
