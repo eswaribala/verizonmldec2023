a0ea4a935ef84c2456d7c40c342dff57382085e4
parameswari.bala <parameswaribala@gmail.com>
2023-12-12 11:26:47 +0530

    updated

--- day2/bargraph.py
+++ day2/bargraph.py
@@ -23,19 +23,19 @@ x_scaled = min_max_scaler.fit_transform(x)
 df = pd.DataFrame(x_scaled)
 print(df)
 
-from sklearn.preprocessing import Normalizer
+# from sklearn.preprocessing import Normalizer
  
-scaler = Normalizer()
-scaled_data = scaler.fit_transform(df)
-scaled_df = pd.DataFrame(scaled_data,columns=df.columns)
-print(scaled_df)
+# scaler = Normalizer()
+# scaled_data = scaler.fit_transform(df)
+# scaled_df = pd.DataFrame(scaled_data,columns=df.columns)
+# print(scaled_df)
 
 fig=plt.figure(figsize=(10,5))
-plt.bar(scaled_df.iloc[:,0],scaled_df.iloc[:,1],color="orange")
+plt.scatter(df.iloc[:,0],df.iloc[:,1],color="red")
 plt.title("Level vs Salary")
 plt.xlabel("Level")
 plt.ylabel("Salary")
-
+#plt.xticks(range(0.0,0.8))
 plt.legend(["Salary","Level"],loc="lower right")
 plt.grid()
 plt.show()
