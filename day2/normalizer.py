cd4115eab47a639583307bc34b3c8631f18761ec
parameswari.bala <parameswaribala@gmail.com>
2023-12-12 10:54:30 +0530

    updated

--- day2/normalizer.py
+++ day2/normalizer.py
@@ -6,19 +6,19 @@ Created on Tue Dec 12 10:41:37 2023
 """
 import pandas as pd
 from matplotlib import pyplot as plt
-df=pd.read_csv("F:/verizonmldec2023/datasets/population.csv")
+df=pd.read_csv("F:/verizonmldec2023/datasets/Position_Salaries.csv")
 pd.set_option('display.max_rows', df.shape[0]+1)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-print(df["Year"],df["Value"])
-df = pd.DataFrame( {"Year":df["Year"], "Population":df["Value"]})
+print(df["Level"],df["Salary"])
+df = pd.DataFrame( {"Level":df["Level"], "Salary":df["Salary"]})
 print(df)
 x = df.values
 
 from sklearn import preprocessing
 
-min_max_scaler = preprocessing.MinMaxScaler()
+
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(x)
 df = pd.DataFrame(x_scaled)
