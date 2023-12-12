253f2126d7a742a196716f752327898e03389ee4
parameswari.bala <parameswaribala@gmail.com>
2023-12-12 15:42:50 +0530

    updated

--- day2/datavisualization.py
+++ day2/datavisualization.py
@@ -27,7 +27,28 @@ plt.show()
 
 
 
-sns.stripplot(x = "Year", y = "Value", data = df.head(n=25))
+sns.boxplot(data = df.head(n=500), orient = "h")
 plt.show()
-
+pkmn_type_colors = ['#78C850',  # Grass
+                    '#F08030',  # Fire
+                    '#6890F0',  # Water
+                    '#A8B820',  # Bug
+                    '#A8A878',  # Normal
+                    '#A040A0',  # Poison
+                    '#F8D030',  # Electric
+                    '#E0C068',  # Ground
+                    '#EE99AC',  # Fairy
+                    '#C03028',  # Fighting
+                    '#F85888',  # Psychic
+                    '#B8A038',  # Rock
+                    '#705898',  # Ghost
+                    '#98D8D8',  # Ice
+                    '#7038F8',  # Dragon
+                   ]
+# Create plot
+sns.violinplot(x='Year',
+               y='Value', 
+               data=df.head(n=15), 
+               inner=None, # Remove the bars inside the violins
+               palette=pkmn_type_colors)
 
